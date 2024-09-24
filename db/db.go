d3a0c3091904056bc4ed1bb302a98360ab46b15b
Jon Crowder <rccommandotac@gmail.com>
2024-09-24 10:30:56 -0500

    CTRL+C to exit, signature and tag db insertion possible, db.EnsureCollection for easier collection creation

--- db/db.go
+++ db/db.go
@@ -14,7 +14,9 @@ import (
 
 type DB struct {
 	pb         *pocketbase.PocketBase
-	signatures *models.Collection
+	Signatures *models.Collection
+	Repos      *models.Collection
+	Tags       *models.Collection
 }
 
 func NewDB() *DB {
@@ -24,9 +26,10 @@ func NewDB() *DB {
 }
 
 func (db *DB) Start(Init func()) error {
-	db.pb.OnAfterBootstrap().Add(func(e *core.BootstrapEvent) error {
+	db.pb.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		err := db.EnsureCollections()
 		if err != nil {
+			log.Println("Issue ensuring collections", err)
 			return err
 		}
 		Init()
@@ -44,121 +47,137 @@ func (db *DB) Start(Init func()) error {
 	return nil
 }
 
+func (db *DB) EnsureCollection(c *models.Collection) (*models.Collection, error) {
+	result, err := db.pb.Dao().FindCollectionByNameOrId(c.Name)
+	if result != nil && err == nil {
+		return result, err
+	}
+	err = db.pb.Dao().SaveCollection(c)
+	if err != nil {
+		return nil, err
+	}
+	return db.pb.Dao().FindCollectionByNameOrId(c.Name)
+}
+
 func (db *DB) EnsureCollections() error {
-	reposCollectionId := ""
 
-	c, err := db.pb.Dao().FindCollectionByNameOrId("repos")
-	if c == nil || err != nil {
-		err = db.pb.Dao().SaveCollection(&models.Collection{
-			Name:       "repos",
-			Type:       models.CollectionTypeBase,
-			ListRule:   types.Pointer(""),
-			ViewRule:   types.Pointer(""),
-			CreateRule: nil, //only admins
-			UpdateRule: nil,
-			DeleteRule: nil,
-			Schema: schema.NewSchema(
-				&schema.SchemaField{
-					Name:     "url",
-					Type:     schema.FieldTypeUrl,
-					Required: true,
-				},
-			),
-		})
-		if err != nil {
-			return err
-		}
-		c, err = db.pb.Dao().FindCollectionByNameOrId("repos")
-	}
-	reposCollectionId = c.Id
-
-	var sigsCollectionId string
-
-	c, err = db.pb.Dao().FindCollectionByNameOrId("signatures")
-	if c == nil || err != nil {
-		db.pb.Dao().SaveCollection(&models.Collection{
-			Name:       "signatures",
-			Type:       models.CollectionTypeBase,
-			ListRule:   types.Pointer("@request.auth.id != ''"),
-			ViewRule:   types.Pointer(""),
-			CreateRule: nil, //only admins
-			UpdateRule: nil,
-			DeleteRule: nil,
-			Schema: schema.NewSchema(
-				&schema.SchemaField{
-					Name:     "name",
-					Type:     schema.FieldTypeText,
-					Required: true,
-				},
-				&schema.SchemaField{
-					Name:     "typestring",
-					Type:     schema.FieldTypeText,
-					Required: true,
-				},
-			),
-			Indexes: types.JsonArray[string]{
-				"CREATE UNIQUE INDEX idx_signature_name ON signatures (name, typestring)",
+	repos, err := db.EnsureCollection(&models.Collection{
+		Name:       "repos",
+		Type:       models.CollectionTypeBase,
+		ListRule:   types.Pointer(""),
+		ViewRule:   types.Pointer(""),
+		CreateRule: nil, //only admins
+		UpdateRule: nil,
+		DeleteRule: nil,
+		Schema: schema.NewSchema(
+			&schema.SchemaField{
+				Name:     "url",
+				Type:     schema.FieldTypeUrl,
+				Required: true,
 			},
-		})
-		c, err = db.pb.Dao().FindCollectionByNameOrId("signatures")
+		),
+	})
+	if err != nil {
+		return err
 	}
-	db.signatures = c
-	log.Println("signatures collection", db.signatures)
-	sigsCollectionId = c.Id
+	db.Repos = repos
 
-	c, err = db.pb.Dao().FindCollectionByNameOrId("tags")
-	if c == nil || err != nil {
-		db.pb.Dao().SaveCollection(&models.Collection{
-			Name:       "tags",
-			Type:       models.CollectionTypeBase,
-			ListRule:   types.Pointer(""),
-			ViewRule:   types.Pointer(""),
-			CreateRule: nil, //only admins
-			UpdateRule: nil,
-			DeleteRule: nil,
-			Schema: schema.NewSchema(
-				&schema.SchemaField{
-					Name:     "name",
-					Type:     schema.FieldTypeText,
-					Required: true,
-				},
-				&schema.SchemaField{
-					Name:     "repo",
-					Type:     schema.FieldTypeRelation,
-					Required: true,
-					Options: schema.RelationOptions{
-						MaxSelect:    types.Pointer(1),
-						CollectionId: reposCollectionId,
-					},
-				},
-				&schema.SchemaField{
-					Name:     "signatures",
-					Type:     schema.FieldTypeRelation,
-					Required: false,
-					Options: schema.RelationOptions{
-						MaxSelect:    nil,
-						CollectionId: sigsCollectionId,
-					},
-				},
-			),
-		})
+	sigs, err := db.EnsureCollection(&models.Collection{
+		Name:       "signatures",
+		Type:       models.CollectionTypeBase,
+		ListRule:   types.Pointer("@request.auth.id != ''"),
+		ViewRule:   types.Pointer(""),
+		CreateRule: nil, //only admins
+		UpdateRule: nil,
+		DeleteRule: nil,
+		Schema: schema.NewSchema(
+			&schema.SchemaField{
+				Name:     "name",
+				Type:     schema.FieldTypeText,
+				Required: true,
+			},
+			&schema.SchemaField{
+				Name:     "typestring",
+				Type:     schema.FieldTypeText,
+				Required: true,
+			},
+		),
+		Indexes: types.JsonArray[string]{
+			"CREATE UNIQUE INDEX idx_signature_name ON signatures (name, typestring)",
+		},
+	})
+	if err != nil {
+		return err
 	}
+	db.Signatures = sigs
+
+	tags, err := db.EnsureCollection(&models.Collection{
+		Name:       "tags",
+		Type:       models.CollectionTypeBase,
+		ListRule:   types.Pointer(""),
+		ViewRule:   types.Pointer(""),
+		CreateRule: nil, //only admins
+		UpdateRule: nil,
+		DeleteRule: nil,
+		Schema: schema.NewSchema(
+			&schema.SchemaField{
+				Name:     "name",
+				Type:     schema.FieldTypeText,
+				Required: true,
+			},
+			&schema.SchemaField{
+				Name:     "repo",
+				Type:     schema.FieldTypeRelation,
+				Required: true,
+				Options: schema.RelationOptions{
+					MaxSelect:    types.Pointer(1),
+					CollectionId: repos.Id,
+				},
+			},
+			&schema.SchemaField{
+				Name:     "signatures",
+				Type:     schema.FieldTypeRelation,
+				Required: false,
+				Options: schema.RelationOptions{
+					MaxSelect:    nil,
+					CollectionId: sigs.Id,
+				},
+			},
+		),
+		Indexes: types.JsonArray[string]{
+			"CREATE UNIQUE INDEX idx_tag ON signatures (name, repo)",
+		},
+	})
+	if err != nil {
+		return err
+	}
+	db.Tags = tags
+
 	return nil
 }
 
-func (db *DB) AddSignatures(TagName string, Signatures ...*proto.Signature) error {
+func (db *DB) AddTag(RepoId string, TagName string, SignatureIds ...string) error {
+	rec := models.NewRecord(db.Tags)
+	rec.Set("repo", RepoId)
+	rec.Set("name", TagName)
+	rec.Set("signatures", SignatureIds)
+	return db.pb.Dao().SaveRecord(rec)
+}
+
+func (db *DB) AddSignatures(Signatures ...*proto.Signature) (error, []string) {
 	errs := []error{}
+	results := []string{}
 	for _, Signature := range Signatures {
-		rec := models.NewRecord(db.signatures)
-		log.Println(Signature.Name, Signature.AsString)
+		rec := models.NewRecord(db.Signatures)
 		rec.Set("name", Signature.Name)
 		rec.Set("typestring", Signature.AsString)
 		err := db.pb.Dao().SaveRecord(rec)
+		results = append(results, rec.Id)
 		if err != nil {
 			errs = append(errs, err)
 		}
 	}
-	return errors.Join(errs...)
+	return errors.Join(errs...), results
 }
 
 func (db *DB) Stop() error {
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"errors"
 	"log"
+	"os"
+	"os/signal"
 	"signachurn/db"
 	"signachurn/scan"
 	"signachurn/scan/cpp"
 	"signachurn/scan/proto"
+	"syscall"
 	"time"
 )
 
@@ -39,11 +42,28 @@ func (s *Scanners) Process(job *proto.ScanJob) (*proto.ScanResult, error) {
 	}
 }
 
+func HandleSigTerm(cb func(sig os.Signal)) {
+	signalChannel := make(chan os.Signal, 2)
+	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		sig := <-signalChannel
+		switch sig {
+		case os.Interrupt:
+			cb(sig)
+		case syscall.SIGTERM:
+			cb(sig)
+		}
+	}()
+}
+
 func main() {
+	HandleSigTerm(func(sig os.Signal) {
+		os.Exit(0)
+	})
+
 	log.Println("starting DB")
 	db := db.NewDB()
 	db.Start(func() {
-
 		log.Println("init scanners")
 
 		scanners := NewScanners()
@@ -64,9 +84,19 @@ func main() {
 		}
 		log.Println("Found", len(scanRes.GetSignatures()), "signatures")
 		log.Println("appending")
-		db.AddSignatures("head", scanRes.GetSignatures()...)
+		err, sigIds := db.AddSignatures(scanRes.GetSignatures()...)
+		if err != nil {
+			// panic(err)
+		}
+		err = db.AddTag(
+			job.Git.RemoteURL,
+			"head",
+			sigIds...,
+		)
 	})
 	defer db.Stop()
 
-	time.Sleep(time.Second * 100)
+	for {
+		time.Sleep(time.Millisecond * 25)
+	}
 }
